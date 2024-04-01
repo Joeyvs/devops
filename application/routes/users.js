1ddaac2b0562d3141301d742c72332ee4a70e74a
Joeyvs <jtj.vansetten@student.avans.nl>
2024-04-01 19:19:11 +0200

    add messagebus connection

--- application/routes/users.js
+++ application/routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const amqp = require('amqplib')
 
 const { db } = require('../services/database')
 
@@ -7,12 +8,29 @@ const { db } = require('../services/database')
 router.get('/', async function (req, res) {
   const users = await db.collection('users').find().toArray()
   res.json(users)
+
+  const connection = await amqp.connect(process.env.MESSAGE_QUEUE)
+  const channel = await connection.createChannel();
+  await channel.assertQueue('logs', { durable: true });
+
+  channel.sendToQueue('logs', Buffer.from(`Received a request to get all users`))
 })
 
-router.post('/', function (req, res) {
+router.post('/', async function (req, res) {
+  const connection = await amqp.connect(process.env.MESSAGE_QUEUE)
+  const channel = await connection.createChannel();
+  await channel.assertQueue('logs', { durable: true });
+
   db.collection('users').insertOne(req.body)
-    .then((user) => res.status(201).json({ id: user.insertedId }))
-    .catch(err => res.status(500).json(err))
+    .then((user) => {
+      res.status(201).json({ id: user.insertedId })
+      channel.sendToQueue('logs', Buffer.from(`User created with ID: ${user.insertedId} `))
+    })
+    .catch(err => {
+      res.status(500).json(err)
+      channel.sendToQueue('logs', Buffer.from(`Failed to create user: ${err}`))
+    })
+    
 })
 
 module.exports = router
---
+++ logger/logs.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const amqp = require('amqplib');
+
+async function start() {
+  const connection = await amqp.connect(process.env.MESSAGE_QUEUE);
+  const channel = await connection.createChannel();
+  await channel.assertQueue('logs', { durable: true });
+  await channel.prefetch(1);
+
+  channel.consume('logs', (msg) => {
+    console.log(msg.content.toString());
+    fs.appendFile('/logs/logs.txt', msg.content.toString() + '\n', err => channel.ack(msg));
+    console.log('Log saved');
+  });
+}
+
+start();
