597c6e4dbec8d4cda3149b9279ab0346352c118c
Joey van Setten <63704989+Joeyvs@users.noreply.github.com>
2024-04-07 14:01:17 +0200

    Add timeout to db query (#14)

--- application/services/database.js
+++ application/services/database.js
@@ -2,10 +2,47 @@ const { MongoClient } = require("mongodb");
 
 const uri = process.env.MONGO_URL;
 
-const client = new MongoClient(uri);
-
-const db = client.db(process.env.DB_NAME || "application");
-module.exports = {
-    db: db,
-    client: client
+const connectToDatabase = async () => {
+    const client = new MongoClient(uri);
+    const db = client.db(process.env.DB_NAME || "application");
+    return {
+        db: db,
+        client: client
+    };
 };
+
+const getUsers = async () => {
+    try {
+        const { db, client } = await connectToDatabase();
+        await client.connect();
+        console.log("Connected to MongoDB")
+        const users = await db.collection('users').find().toArray();
+        client.close();
+        return users;
+    } catch(err) {
+        console.error('Could not connect to MongoDB:', err);
+        return [];
+    }
+};
+
+const postUser = async (json) => {
+    try {
+        const { db, client } = await connectToDatabase();
+        await client.connect();
+        console.log("Connected to MongoDB")
+        const userId = await db.collection('users').insertOne(json)
+            .then(async (user) => {
+                return user.insertedId;
+            })
+            .catch(async () => {
+                return 0;
+            });
+        client.close();
+        return userId;
+    } catch(err) {
+        console.error('Could not connect to MongoDB:', err);
+        return false;
+    }
+};
+
+module.exports = { getUsers, postUser };
